f0a3dcbb6572d5401a6c464f9a0f152b19dac0f4
Mustak Ahmmed <mustakahmmed793@gmail.com>
2025-02-10 23:02:17 +0600

    immutable, mutable values and cases.

---
+++ p-string.js
@@ -0,0 +1,9 @@
+// string Vs Array: string is immutable while array is mutable
+
+let greeting = "Hi, good evening!";
+let firstLetter = greeting[0].toLowerCase();
+
+// tolowercase
+console.log("Lowercase text: ", greeting.toLowerCase());
+console.log("The greeting: ", greeting);
+console.log("first Letter: ", firstLetter);
