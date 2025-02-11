e730cc0c613736bb8528dabd1b1f41b4c043fb94
Mustak Ahmmed <mustakahmmed793@gmail.com>
2025-02-11 11:50:07 +0600

    for loop for array

--- p-array.js
+++ p-array.js
@@ -1,1 +1,8 @@
 let array = [20, 40, 30, 50, 12, 42, 322, 44];
+console.log("array 1: ", array[1]);
+
+// for loop for the array
+for(let i = 0; i< array.length; i++){
+    console.log("index: ", i);
+    console.log(array[i]);
+}
