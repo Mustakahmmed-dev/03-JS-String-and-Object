817db793da54992fc7f65ddb71fbf5337bc0599a
Mustak Ahmmed <mustakahmmed793@gmail.com>
2025-02-11 11:54:42 +0600

    while loop in array

--- p-array.js
+++ p-array.js
@@ -3,6 +3,19 @@ console.log("array 1: ", array[1]);
 
 // for loop for the array
 for(let i = 0; i< array.length; i++){
-    console.log("index: ", i);
-    console.log(array[i]);
+    // console.log("index: ", i);
+    // console.log(array[i]);
+}
+
+// using while loop in array
+let y = 0;
+while(y < array.length){
+    if(y > 3){
+        console.log("Break now");
+        break;
+
+    }
+    console.log(y, " : ", array[y]);
+    
+    y++;
 }
